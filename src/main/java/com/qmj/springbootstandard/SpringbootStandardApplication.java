2624d82883f229b76b88cffe7b41afbbc3f74384
qmjgithub <qmj123abc@163.com>
2018-11-30 11:03:54 +0800

    创建项目

---
+++ src/main/java/com/qmj/springbootstandard/SpringbootStandardApplication.java
@@ -0,0 +1,12 @@
+package com.qmj.springbootstandard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStandardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStandardApplication.class, args);
+    }
+}
---
+++ src/test/java/com/qmj/springbootstandard/SpringbootStandardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qmj.springbootstandard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootStandardApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
